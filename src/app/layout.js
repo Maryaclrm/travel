1c69dbbe4266114599e6ae6593b45d38613ebb21
Marya Clara <mcmaryas@gmail.com>
2023-10-03 16:22:55 -0300

    textos

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,9 +27,23 @@ function Menu({children, x, y}){
   );
 } 
 
+
+
 function ItemMenu({nome}){
   return(
     <p className={styles.ItemMenu}>{nome}</p>
+  );
+}
+
+function Maior({textin, x, y}){
+  return(
+    <p style= {{left: x, top:y}} className={styles.textoMaior}>{textin}</p>
+  );
+}
+
+function Menor({textan, x, y}){
+  return(
+    <p style= {{left:x, top: y}} className={styles.textoMenor}>{textan}</p>
   );
 }
 
@@ -41,17 +55,22 @@ export default function RootLayout({ children }) {
           <Logo x={135} y={53} ativo={{nome: 'fulano', idade:'25', aluno:''}}/>  
           <Menu x={435} y={60}>
             <ItemMenu nome="Home"/>
-          <ItemMenu nome="About"/>
-          <ItemMenu nome="Contact"/>
-          <ItemMenu nome="Blog"/>
-          <ItemMenu nome="Videos"/>
-          <ItemMenu nome="+38(097)8849989"/>
+            <ItemMenu nome="About"/>
+            <ItemMenu nome="Contact"/>
+            <ItemMenu nome="Blog"/>
+            <ItemMenu nome="Videos"/>
+            <ItemMenu nome="+38(097)8849989"/>
           </Menu>
+        </header>
           <main>
+          <Maior  x={135} y={238} textin="Your perfect honeymoon"/>
+          <Menor x={136} y={465.04} textan="Mayami is more than a honeymoon destination –
+          it’s a perfect romantic spot for couples
+           who like  spending time in beach. "/>
           {children}
           </main>
-        </header>
         </body>
+        
     </html>
   )
 }
