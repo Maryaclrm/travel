93b7c27ab50ba076e69f809d6e0b2f5ea80b9b55
Marya Clara <mcmaryas@gmail.com>
2023-10-19 14:18:19 -0300

    botao start

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
 import styles from './page.module.css'
 
+function Botao({x,y}){
+ return (
+    <button style={{left:x, top:y}} className={styles.botao}>Start</button>
+  )
+}
+
 export default function Home() {
   return (
-    <h1></h1>
-  )}
+    <Botao x={135} y={601.72}/>
+  )
+}
